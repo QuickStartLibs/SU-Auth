604b61adfdcef3ef599a913da8d904dfc68a6b10
Travis van der F <travis.font@gmail.com>
2019-03-17 21:51:49 +0100

    new config setup

---
+++ src/config.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SUAuth\Config
+{
+
+    abstract class Server
+    {
+        const Server      = 'localhost';
+        const Database    = 'user';
+        const DB_User     = 'root';
+        const DB_Password = '';
+    }
+
+    abstract class Field extends Server
+    {
+        const Handle   = 'email';
+        const Password = 'password';
+    }
+
+    abstract class Cookie extends Field
+    {
+        const Name = 'user';
+    }
+
+    abstract class Config extends Cookie
+    {
+    }
+}
